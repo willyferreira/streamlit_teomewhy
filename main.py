1701c0d70e818538e36959b8a5bfed040ffd22ec
Willy Ferreira <40251830+willyferreira@users.noreply.github.com>
2025-11-06 14:43:18 -0300

    fim da aula 3

--- main.py
+++ main.py
@@ -25,7 +25,39 @@ if file_upload:
 
      # Leitura dos dados
      df = pd.read_csv(file_upload)
+     df["Data"] = pd.to_datetime(df["Data"], format = "%d/%m/%Y").dt.date
 
      # Exibição dos dados do app
+     exp1 = st.expander("Dados Brutos")
      columns_fmt = {"Valor" : st.column_config.NumberColumn("Valor", format="R$ %f")}
-     st.dataframe(df, hide_index= True, column_config= columns_fmt)
+     exp1.dataframe(df, hide_index= True, column_config= columns_fmt)
+
+     # Visão Instituição
+     exp2 = st.expander("Instituições")
+     df_instituicao = df.pivot_table(index = "Data", columns = "Instituição", values = "Valor")
+
+     tab_data, tab_history, tab_share = exp2.tabs(["Dados", "Histórico", "Distribuição"])
+
+     with tab_data:
+          st.dataframe(df_instituicao)
+     
+     with tab_history:
+          st.line_chart(df_instituicao)
+
+     with tab_share:
+          
+          date = st.selectbox("Filtro Data", options = df_instituicao.index)
+          # Input de data
+          # date = st.date_input("Data para Distribuição",
+          #                      min_value=df_instituicao.index.min(),
+          #                      max_value=df_instituicao.index.max()
+          #                      )
+          # Condição caso seja inputada data inválida
+          # if date not in df_instituicao.index:
+          #      st.warning("Entre com uma data válida")
+          # else:
+               # Obtém a última data de dados
+               # last_dt = df_instituicao.sort_index().iloc[-1]  
+          st.bar_chart(df_instituicao.loc[date])
+
+# Não tem arquivos...
